f9f5d67f2f02699a6d10acb53b59f6916c0a8d41
Jeremy Kitchen <kitchen@kitchen.io>
2019-05-23 09:03:49 -0700

    Initial Commit

---
+++ KissTNC/KissTNC.h
@@ -0,0 +1,19 @@
+//
+//  KissTNC.h
+//  KissTNC
+//
+//  Created by Jeremy Kitchen on 5/23/19.
+//  Copyright © 2019 Jeremy Kitchen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KissTNC.
+FOUNDATION_EXPORT double KissTNCVersionNumber;
+
+//! Project version string for KissTNC.
+FOUNDATION_EXPORT const unsigned char KissTNCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissTNC/PublicHeader.h>
+
+
