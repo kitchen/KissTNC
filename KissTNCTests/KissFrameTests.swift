b4a75dd90d0a5266dc6c561646ccfdeb4321dd1a
Jeremy Kitchen <kitchen@kitchen.io>
2019-05-24 20:33:25 -0700

    swap expected, actual, and some whitespace cleanup

--- KissTNCTests/KissFrameTests.swift
+++ KissTNCTests/KissFrameTests.swift
@@ -11,36 +11,36 @@ import XCTest
 class KissFrameTests: XCTestCase {
     func testParseFrames() {
         var frame = KissFrame(Data(base64Encoded: "wACEioKGnpxgrm6YqEBAdQPwUEFSQyBXSU5MSU5LIEdBVEVXQVkgT04gTVQgU0NPVFQsIENOODVSSywgUkVQRUFURVIgT04gMTQ2Ljg0IC02MDAsIElORk9AVzdMVC5PUkcNwA==")!)
-        XCTAssertEqual(frame.command, 0x00, "the command is a data packet")
-        XCTAssertEqual(frame.port, 0, "the port is 0")
-        XCTAssertEqual(frame.payload.count, 97, "the length of the payload is 97")
-        XCTAssertNotEqual(frame.payload.last, KissFrame.FEND, "we strip off the trailing FEND")
-        
+        XCTAssertEqual(0x00, frame.command, "the command is a data packet")
+        XCTAssertEqual(0, frame.port, "the port is 0")
+        XCTAssertEqual(97, frame.payload.count, "the length of the payload is 97")
+        XCTAssertNotEqual(KissFrame.FEND, frame.payload.last, "we strip off the trailing FEND")
+
         frame = KissFrame(Data([KissFrame.FEND, 0xff, KissFrame.FEND]))
-        XCTAssertEqual(frame.command, KissFrame.Return, "the command is end KISS")
-        XCTAssertEqual(frame.port, 0x0F, "the port is F")
-        XCTAssertEqual(frame.payload.count, 0, "there is no payload")
-        
+        XCTAssertEqual(KissFrame.Return, frame.command, "the command is end KISS")
+        XCTAssertEqual(0x0f, frame.port, "the port is F")
+        XCTAssertEqual(0, frame.payload.count, "there is no payload")
+
         frame = KissFrame(Data([KissFrame.FEND, 0x00, KissFrame.FESC, KissFrame.TFEND, KissFrame.FESC, KissFrame.TFESC, KissFrame.FEND]))
-        XCTAssertEqual(frame.payload, Data([KissFrame.FEND, KissFrame.FESC]), "ensure we are properly unescaping")
+        XCTAssertEqual(Data([KissFrame.FEND, KissFrame.FESC]), frame.payload, "ensure we are properly unescaping")
 
         frame = KissFrame(Data([KissFrame.FEND, 0x00, 0x01, 0x01, 0x01, KissFrame.FESC, KissFrame.TFEND, 0x01, 0x01, 0x01, KissFrame.FESC, KissFrame.TFESC, 0x01, 0x01, 0x01, 0x01, KissFrame.FEND]))
-        XCTAssertEqual(frame.payload, Data([0x01, 0x01, 0x01, KissFrame.FEND, 0x01, 0x01, 0x01, KissFrame.FESC, 0x01, 0x01, 0x01, 0x01]))
+        XCTAssertEqual(Data([0x01, 0x01, 0x01, KissFrame.FEND, 0x01, 0x01, 0x01, KissFrame.FESC, 0x01, 0x01, 0x01, 0x01]), frame.payload)
 
         // assume something already cut off the FENDs
         frame = KissFrame(Data([0x00, 0x01, 0x02, 0x03]))
         XCTAssertEqual(0x00, frame.command)
         XCTAssertEqual(Data([0x01, 0x02, 0x03]), frame.payload)
     }
-    
+
     func testGenerateFrames() {
         var frame = KissFrame(port: 0x00, command: 0x00, payload: Data([0x00,0x01,0x02,0x03]))
-        XCTAssertEqual(frame.frame(), Data([KissFrame.FEND, 0x00, 0x00, 0x01, 0x02, 0x03, KissFrame.FEND]))
-        
+        XCTAssertEqual(Data([KissFrame.FEND, 0x00, 0x00, 0x01, 0x02, 0x03, KissFrame.FEND]), frame.frame())
+
         frame = KissFrame(port: 0x00, command: 0x00, payload: Data([KissFrame.FEND, KissFrame.FESC]))
-        XCTAssertEqual(frame.frame(), Data([KissFrame.FEND, 0x00, KissFrame.FESC, KissFrame.TFEND, KissFrame.FESC, KissFrame.TFESC, KissFrame.FEND]), "ensure we are properly escaping")
+        XCTAssertEqual(Data([KissFrame.FEND, 0x00, KissFrame.FESC, KissFrame.TFEND, KissFrame.FESC, KissFrame.TFESC, KissFrame.FEND]), frame.frame(), "ensure we are properly escaping")
     }
-    
+
     func testRoundTrips() {
         let framesToRoundTrip = [
             KissFrame(Data([KissFrame.FEND, 0x00, 0x00, 0x01, 0x02, 0x03, KissFrame.FEND])),
@@ -50,12 +50,9 @@ class KissFrameTests: XCTestCase {
         for frame in framesToRoundTrip {
             let frame2 = KissFrame(port: frame.port, command: frame.command, payload: frame.payload)
             let frame3 = KissFrame(frame2.frame())
-            
-            XCTAssertEqual(frame.frame(), frame2.frame(), "they're all the same")
-            XCTAssertEqual(frame.frame(), frame3.frame(), "they're all the same")
-        }
-        
-        
-    }
 
+            XCTAssertEqual(frame2.frame(), frame.frame(), "they're all the same")
+            XCTAssertEqual(frame3.frame(), frame.frame(), "they're all the same")
+        }
+    }
 }
